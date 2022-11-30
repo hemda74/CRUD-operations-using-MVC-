9f4fbd480f6d6d8719e613216dcfc34093a7ea08
Ahmed Hemdan <92821656+hemda74@users.noreply.github.com>
2022-12-01 01:50:54 +0200

    First Commit

---
+++ DotNetTask/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using CURDOprationsDimo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNetTask.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            //modelBuilder.Entity<Employee>().ToTable("Employee","HR");
+        }
+        public DbSet<Department> Departments { get; set; } 
+        public DbSet<Employee> Employees { get; set; }  
+    }
+}
